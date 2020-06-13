d6b82d22d32fdb560b4d412a1565ac22b10876b6
Tammy Lam <lamtammy1515@gmail.com>
2020-06-13 19:53:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students
-  resources :school_classes
+  resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
